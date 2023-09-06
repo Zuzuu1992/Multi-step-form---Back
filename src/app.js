7d929d741607ed98459cf3ea1dd9f00ce36a0143
Ana Izoria <ana.izoria999@gmail.com>
2023-09-06 20:24:19 +0400

    add database

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import dotenv from "dotenv";
+import connect from "./databases/mongo.js";
 
 dotenv.config();
 connect();
